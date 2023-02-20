5123096633818a0e86b4632370cb5f4f1b00c570
DucNDA <justakidz1402@gmail.com>
2023-02-21 00:11:00 +0700

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Node.js');
-}).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000');
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello Node.js');
+// }).listen(3000, "127.0.0.1");
+// console.log('Server running at http://127.0.0.1:3000');
+
+$(document.ready(function(){
+    alert('hello');
+}));
